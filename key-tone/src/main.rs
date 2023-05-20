09f9e522815cc6b1cfb13ed2a27fb99a13170d59
沫烬染 <3341588907@qq.com>
2023-05-20 16:24:31 +0800

    修复按住一个按键后，再去按其他按键没声音

--- key-tone/src/main.rs
+++ key-tone/src/main.rs
@@ -1,32 +1,38 @@
 #![windows_subsystem = "windows"]
 
-extern crate rodio;
 extern crate device_query;
+extern crate rodio;
 extern crate tinyfiledialogs;
 
-use std::fs::{File, metadata};
-use std::io::{Read, Cursor};
-use rodio::{OutputStream, source::Source};
-use device_query::{DeviceQuery, DeviceState, Keycode};
+use device_query::{DeviceQuery, DeviceState};
+use rodio::{source::Source, OutputStream};
+use std::fs::{metadata, File};
+use std::io::{Cursor, Read};
 use tinyfiledialogs::message_box_ok;
 
 fn main() {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let device_state = DeviceState::new();
-    let mut prev_keys: Vec<Keycode> = Vec::new();
+
     if metadata("ding.wav").is_err() {
-        message_box_ok("Error", "File not found", tinyfiledialogs::MessageBoxIcon::Error);
+        message_box_ok(
+            "Error",
+            "File not found",
+            tinyfiledialogs::MessageBoxIcon::Error,
+        );
         return;
     }
     let mut file = File::open("ding.wav").unwrap();
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).unwrap();
+
+    let mut prev_keys_len = 0;
     loop {
-        let keys: Vec<Keycode> = device_state.get_keys();
-        if !keys.is_empty() && prev_keys.is_empty() {
+        let keys_len = device_state.get_keys().len();
+        if keys_len > prev_keys_len {
             let source = rodio::Decoder::new(Cursor::new(buffer.clone())).unwrap();
             stream_handle.play_raw(source.convert_samples()).unwrap();
         }
-        prev_keys = keys;
+        prev_keys_len = keys_len;
     }
 }
