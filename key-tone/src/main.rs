b7c979968d09b50f643b146ce9a5b7b02d29f971
xy_cloud <85053859+xy-cloud-cn@users.noreply.github.com>
2023-05-20 19:39:37 +0800

    fix issue#1

--- key-tone/src/main.rs
+++ key-tone/src/main.rs
@@ -6,6 +6,7 @@ extern crate tinyfiledialogs;
 
 use std::fs::{File, metadata};
 use std::io::{Read, Cursor};
+use std::collections::HashSet;
 use rodio::{OutputStream, source::Source};
 use device_query::{DeviceQuery, DeviceState, Keycode};
 use tinyfiledialogs::message_box_ok;
@@ -13,7 +14,6 @@ use tinyfiledialogs::message_box_ok;
 fn main() {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
     let device_state = DeviceState::new();
-    let mut prev_keys: Vec<Keycode> = Vec::new();
     if metadata("ding.wav").is_err() {
         message_box_ok("Error", "File not found", tinyfiledialogs::MessageBoxIcon::Error);
         return;
@@ -21,12 +21,14 @@ fn main() {
     let mut file = File::open("ding.wav").unwrap();
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).unwrap();
+    let mut prev_keys_set: HashSet<Keycode> = HashSet::new();
     loop {
         let keys: Vec<Keycode> = device_state.get_keys();
-        if !keys.is_empty() && prev_keys.is_empty() {
+        let keys_set: HashSet<Keycode> = keys.into_iter().collect();
+        for _key in keys_set.difference(&prev_keys_set) {
             let source = rodio::Decoder::new(Cursor::new(buffer.clone())).unwrap();
             stream_handle.play_raw(source.convert_samples()).unwrap();
         }
-        prev_keys = keys;
+        prev_keys_set = keys_set;
     }
 }
